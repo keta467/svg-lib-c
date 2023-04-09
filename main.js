9275ffc26d645eaee470b95de9459442a919b3b5
IRIE-Z4G4\irie <irie@lambda.co.jp>
2023-04-09 19:35:46 +0900

    message

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-class TestClass {
+export class TestClass {
   sayHello() {
     console.log("hello!!!!!変更2");
   }
 }
 
-class RectTag {
+export class RectTag {
   getScript() {
     return "aiueo";
   }
 }
-
-export default {
-  TestClass,
-  RectTag,
-};
 
 //これ単体で動くアプリをつくりたいが、、
 // document.getElementById("root").innerHTML += "dddddd";
