4941bb7736a6ecc40464c224c7a55c1ced5c548f
IRIE-Z4G4\irie <irie@lambda.co.jp>
2023-04-09 21:18:18 +0900

    message

--- lib.js
+++ lib.js
@@ -1,14 +1,14 @@
-// import {
-//   BaseTag,
-//   CircleTag,
-//   GroupTag,
-//   ImageTag,
-//   RectTag,
-//   SvgTag,
-//   TextTag,
-// } from "./basetag.js";
+import {
+  BaseTag,
+  CircleTag,
+  GroupTag,
+  ImageTag,
+  RectTag,
+  SvgTag,
+  TextTag,
+} from "./basetag.js";
 
-// export { BaseTag, RectTag, SvgTag, CircleTag, GroupTag, ImageTag, TextTag };
+export { BaseTag, RectTag, SvgTag, CircleTag, GroupTag, ImageTag, TextTag };
 
 function myFunction() {
   console.log("Hello from my-library!");
